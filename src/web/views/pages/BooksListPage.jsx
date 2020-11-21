f3c88d77cefa1a7939a3b0a3f6e0c1e3243ee871
Ravi <rav-pradhan@users.noreply.github.com>
2020-11-21 22:56:51 +0000

    flip loading logic

--- src/web/views/pages/BooksListPage.jsx
+++ src/web/views/pages/BooksListPage.jsx
@@ -3,34 +3,39 @@ import React, { useState, useEffect } from "react";
 
 export default ({ Layout, fetchBooks, toggleBookReadStatus }) => {
   return () => {
-    const [isLoading, setLoading] = useState(false);
+    const [isLoading, setLoading] = useState(true);
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
       fetchBooks.invoke().then((res) => {
         setBooks(res);
-        setLoading(true);
+        setLoading(false);
       });
     }, [setBooks]);
 
     const renderBooksList = () => (
       <ul>
-        {books.map(book => (
+        {books.map((book) => (
           <li key={book.id}>
             {book.title} - {book.author} (read? {renderReadStatus(book)})
           </li>
         ))}
       </ul>
-    )
+    );
 
     const renderReadStatus = (book) => (
-      <input onClick={() => toggleBookReadStatus.invoke(book.id, book.hasRead)} type="checkbox" defaultChecked={book.hasRead} value={book.hasRead}/>
-    )
+      <input
+        onClick={() => toggleBookReadStatus.invoke(book.id, book.hasRead)}
+        type="checkbox"
+        defaultChecked={book.hasRead}
+        value={book.hasRead}
+      />
+    );
 
     return (
       <Layout>
         <h2>Books</h2>
-        {isLoading ? renderBooksList() : <p>Loading...</p>}
+        {isLoading ? <p>Loading...</p> : renderBooksList()}
       </Layout>
     );
   };
